de5c3b33b557e8f0357f6c9d3709001d6d3677a3
m-strzalkowski <strzamat@gmail.com>
2023-05-16 22:12:11 +0200

    Pierwsze próby importów

--- win_import.bat
+++ win_import.bat
@@ -1,4 +1,7 @@
 echo all: %*
 IF "%1"=="" EXIT /b
+IF "%piip%" == "" win_szukaj.bat
+IF "%piip%" == "" EXIT /b
 for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
 ssh ms@%piip% -i .ssh\pi "cd smacz/rejestrator; ./eksportuj" >importowane\%1.csv
+ECHO "import ended"
